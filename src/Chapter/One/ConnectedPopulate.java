cb70f5f03d0e871e20008738c65d383a48c4050a
Todd Gerspacher <toddgerspacher27@gmail.com>
2013-02-26 16:27:46 -0500

    refactored file

--- src/Chapter/One/ConnectedPopulate.java
+++ src/Chapter/One/ConnectedPopulate.java
@@ -1,67 +1,68 @@
-package com.hibernatebook.chapter01;
+package Chapter.One;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
-/**
- * Populates the HSQL Database with a message
- */
+//Title: Pro Hibernate 3
+//Authors: Dave Minter, Jeff Linwood
+//Chapter 1 : An Introduction to Hibernate 3
+
+// Populates the HSQL Database with a message
 public class ConnectedPopulate {
 
-   public static void main(String[] args) {
-      try {
-         setMotd(42,"This is the message of the day");
-      } catch (MotdException e) {
-         System.err.println("Couldn't get the message: " + e);
+  public static void main(String[] args) {
+    try {
+      Class.forName("com.mysql.jdbc.Driver").newInstance();
+    }
+    catch (ClassNotFoundException classNotFound) {
+      classNotFound.printStackTrace();
+      return;
+    }
+    catch (IllegalAccessException illegalAccess) {
+      illegalAccess.printStackTrace();
+      return;
+    }
+    catch (InstantiationException instantiation) {
+      instantiation.printStackTrace();
+      return;
+    }
+
+    Connection connection = null;
+    PreparedStatement preparedStatement = null;
+
+    try {
+      connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/my_jdbc", "root", "password");
+      preparedStatement = connection.prepareStatement("insert into MessageOfTheDay (id, message) values (?, ?)");
+
+      preparedStatement.setInt(1, 42); // messageId
+      preparedStatement.setString(2, "This is the message of the day"); // message
+      preparedStatement.executeUpdate();
+    }
+    catch (SQLException sql) {
+      sql.printStackTrace();
+      return;
+    }
+    finally {
+      if (preparedStatement != null) {
+        try {
+          preparedStatement.close();
+        }
+        catch (SQLException sql) {
+          sql.printStackTrace();
+        }
       }
-   }
 
-   public static void setMotd(int messageId,String message)
-      throws MotdException
-   {
-      Connection c = null;
-      PreparedStatement p = null;
-
-      try {
-
-         // This would tend to reduce to a single line from three
-         Class.forName("org.hsqldb.jdbcDriver");
-         c = DriverManager.getConnection("jdbc:hsqldb:file:messagedb", "sa", "");
-         p = c.prepareStatement("insert into motd (id,message) values (?,?)");
-
-         p.setInt(1, messageId);
-         p.setString(2,message);
-         p.executeUpdate();
-
-      } catch (Exception e) {
-         log.log(Level.SEVERE, "Could not store message", e);
-         throw new MotdException(
-               "Failed to store message in the database.", e);
-      } finally {
-         if (p != null) {
-            try {
-               p.close();
-            } catch (SQLException e) {
-               log.log(Level.WARNING,
-                     "Could not close ostensibly open statement.", e);
-            }
-         }
-
-         if (c != null) {
-            try {
-               c.close();
-            } catch (SQLException e) {
-               log.log(Level.WARNING,
-                     "Could not close ostensibly open connection.", e);
-            }
-         }
+      if (connection != null) {
+        try {
+          connection.close();
+        }
+        catch (SQLException sql) {
+          sql.printStackTrace();
+          return;
+        }
       }
-   }
-
-   private static final Logger log = Logger.getAnonymousLogger();
+    }
+  }
 }
