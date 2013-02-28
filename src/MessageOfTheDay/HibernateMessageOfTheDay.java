b853a7729fc5517661b7b33cc8981004dad6d379
Todd Gerspacher <toddgerspacher27@gmail.com>
2013-02-28 10:11:41 -0500

    * refactored HibernateMessageOfTheDay * added documentation comments to HibernateMessageOfTheDay

--- src/MessageOfTheDay/HibernateMessageOfTheDay.java
+++ src/MessageOfTheDay/HibernateMessageOfTheDay.java
@@ -2,7 +2,8 @@ package MessageOfTheDay;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.AnnotationConfiguration;
 
 import Plain_Old_Java_Objects.MessageOfTheDay;
 
@@ -12,17 +13,28 @@ import Plain_Old_Java_Objects.MessageOfTheDay;
 public class HibernateMessageOfTheDay {
 
   public static void main(String[] args) {
-    SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
-    Session session = sessionFactory.openSession();
+    // Configure Hibernate so it can handle the annotated MessageOfTheDay JavaBean
+    // (page 66, "Hibernate Made Easy", Cameron McKenzie)
+    AnnotationConfiguration config = new AnnotationConfiguration();
+    config.addAnnotatedClass(MessageOfTheDay.class);
+    config.configure();
+
+    SessionFactory sessionFactory = config.buildSessionFactory();
+    Session session = sessionFactory.getCurrentSession();
+    // Note: SessionFactory#openSession creates a Session object
+
+    Transaction transaction = session.beginTransaction();
     MessageOfTheDay messageOfTheDay = (MessageOfTheDay) session.get(MessageOfTheDay.class, 42);
+    transaction.commit();
 
     if (messageOfTheDay != null) {
-      System.out.println(messageOfTheDay.getMessage());
+      System.out.println(messageOfTheDay.getId() + ": " + messageOfTheDay.getMessage());
     }
     else {
       System.out.println("No such message");
     }
 
-    session.close();
+    // session.close();
+    // throws org.hibernate.SessionException: Session was already closed
   }
 }
