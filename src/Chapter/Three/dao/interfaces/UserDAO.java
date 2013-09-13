73a3dd5481fa6ca1c4e5051e619c3efb2d86e5af
Todd Gerspacher <toddgerspacher27@gmail.com>
2013-09-13 06:10:06 -0400

    Deleted Chapter 3 files.

--- src/Chapter/Three/dao/interfaces/UserDAO.java
+++
@@ -1,8 +0,0 @@
-package Chapter.Three.dao.interfaces;
-
-import Chapter.Three.Cameron_Mckenzie.GenericDAO;
-import Chapter.Three.pojo.User;
-
-public interface UserDAO extends GenericDAO<User, Long> {
-  public User createUser(String username, String password);
-}
